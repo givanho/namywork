d0af85e0b91ece3fdc960947de47df19934ba676
Givanho <fynefaceg@gmail.com>
2023-09-03 14:27:40 +0100

    Added functionalities to this screen

--- Screens/EditScreen.js
+++ Screens/EditScreen.js
@@ -1,195 +1,310 @@
 import React, {useState, useEffect} from 'react'
 import { Ionicons } from '@expo/vector-icons';
-import { Box, Image, Center ,Text, Input,Button,
-   AspectRatio, Stack,HStack,Flex,VStack, Spacer, Link,
-   FormControl,Heading} from 'native-base'
+import { Box,  Center ,Text, Input,Button,
+   HStack,VStack,
+   FormControl,Avatar, ScrollView, TextArea} from 'native-base'
    import { UserAuth } from '../context/context'
 import * as ImagePicker from 'expo-image-picker' 
-import {ScrollView, View, TouchableOpacity} from 'react-native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {TouchableOpacity} from 'react-native';
+import { collection, query, where ,doc, setDoc, onSnapshot, } from "firebase/firestore";
+import { db , storage} from '../firebase';
+import { ref , uploadBytesResumable, getDownloadURL,} from 'firebase/storage';
+import DropDownMenu from '../Components/DropDownMenu';
 
-const EditScreen = () => {
-  const {createUser, googleSignIn, user, logout} = UserAuth();
 
-    const [number, setNumber]= useState('')
-    const [skill, setSkill]= useState('')
-    const [about, setAbout]= useState('')
-    const [location, setLocation]= useState('')
+const EditScreen = ({navigation, route}) => {
+  const{value, locationRoute, aboutRoute, skillRoute} = route.params
+  const { user} = UserAuth();
+  const [data, setData] = useState(null)
+  const maxLength = 200;
+    const [number, setNumber]= useState(value)
+    const [skill, setSkill]= useState(skillRoute)
+    const [about, setAbout]= useState(aboutRoute)
+    const [location, setLocation]= useState(locationRoute)
+    const [userImgSrc, setUserImgSrc] = useState(null);
+    const [image, setImage] = useState(null)
+    const [imgSrc, setImgSrc] = useState(require("../assets/avatar.png"));
 
-    const [images, setImages] = useState("https://img.icons8.com/?size=1x&id=23265&format=png")
-const [image, setImage] = useState('')
+    const [selectedState, setSelectedState] = useState(null);
+  const [selectedCapital, setSelectedCapital] = useState(null);
+
 const [handlePermissions, setHandlePermissions] = useState(null)
 
-// Hook for Profile picture Permission on device
-useEffect(() => {
-    (async() =>{
-      const galleryStatus = await ImagePicker.requestMediaLibraryPermissionsAsync();
-      setHandlePermissions(galleryStatus.status === 'granted');
-    })()
+
+
+//imageupload function
+
+const imageUpload = async () =>{
+  if( userImgSrc == null ) {
+    await setDoc(doc(db, 'users', user.uid), {
+     
+      number,
+      location,
+      about,
+      skill,
+      }, { merge: true });
+      navigation.goBack();
+  }
   
-    
-  }, []);
-  
-  
-  // Function that handle user image Picker (profile pic)
-  const pickImage = async () => {
-      // No permissions request is necessary for launching the image library
-      let result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.Images,
-        allowsEditing: true,
-        aspect: [4, 3],
-        quality: 1,
-      });
-  
-      console.log(result);
-  
-      if (!result.canceled) {
-        setImage(result.assets[0].uri);
+else{
+
+
+ 
+  try {
+    const storageRef = ref(storage, 'profile_pics/' + user.email + '/DP');
+    const response = await fetch(userImgSrc);
+    const blob = await response.blob();
+
+    const uploadTask = uploadBytesResumable(storageRef, blob);
+
+    uploadTask.on(
+      'state_changed',
+      // ... progress and error handlers ...
+      async() => {
+        // Handle successful uploads on complete
+        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+      return await getDownloadURL(uploadTask.snapshot.ref)
+      .then(async (downloadURL) => {
+        setImage(downloadURL)
+          console.log('File available at', downloadURL);
+
+         await setDoc(doc(db, 'users', user.uid), {
+                 userImg: downloadURL, // Use downloadURL here
+                 number,
+                 location,
+                 about,
+                 skill,
+                 }, { merge: true });
+        });
       }
-    };
-   if (handlePermissions === false){
-    return <Text>No access to gallery</Text>
-   }
+      
+    );
+
+    // ... rest of your code ...
+    navigation.goBack();
+  } catch (error) {
+    console.error('Error uploading image: ', error);
+  }
+}
+};
+
+
+
+  //We're using the useEffect hook to set up the real-time listener when the component mounts
   useEffect(() => {
-      if (user && user.photoURL) {
-        setEmail(user.email);
-        setDisplayName(user.displayName)
-        setImages(user.photoURL)
-        
-      } else {
-        console.log('No users found');
-      }
-    }, [user]);
+    if (user) {
+      //When the query snapshot changes (new data is added), 
+     // the onSnapshot callback function is called. If the query snapshot is not empty, 
+     // we update the state with the data from the first document in the snapshot.
+
+      const q = query(collection(db, 'users'), where('userID', '==', user.uid));
+
+      //The unsubscribe function returned by onSnapshot is used to 
+      //remove the listener when the component unmounts, preventing memory leaks.
+
+      const unsubscribe = onSnapshot(q, (querySnapshot) => {
+        if (!querySnapshot.empty) {
+          const doc = querySnapshot.docs[0];
+          console.log(doc.id, ' => ', doc.data());
+          setData(doc.data());
+          if (doc.data().userImg) {
+            setImgSrc({ uri: doc.data().userImg });
+          }
+         
+        }
+      });
+
+      return () => {
+        unsubscribe();
+      };
+    }
+  }, [image]);
+
+
+useEffect(() => {
+  (async() =>{
+    const galleryStatus = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    setHandlePermissions(galleryStatus.status === 'granted');
+  })()
+
+  
+}, []);
+
+
+const handleStateSelection = (state) => {
+  setSelectedState(state);
+
+};
+
+// Callback function to update selected capital
+const handleCapitalSelection = (capital) => {
+  setSelectedCapital(capital);
+
+};
+useEffect(() => {
+  if (selectedState&&selectedCapital){
+    setLocation(selectedState+', '+selectedCapital)
+  }
+  else{
+    setLocation(locationRoute)
+  }
+}, [selectedCapital, selectedState])
+
+
+
+// Function that handle user image Picker (profile pic)
+const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 4],
+      quality:0.5
+    });
+
+    
+     console.log('user clicked council')
+    if (!result.canceled) {
+      const email = user.email; // Replace with actual user ID
+      setUserImgSrc(result.assets[0].uri)
+    // await imageUpload(result.assets[0].uri);
+    console.log('userimagesource: =>' + userImgSrc)
+      // setImage(imageUri);
+    }
+  };
+ if (handlePermissions === false){
+  return <Text>No access to gallery</Text>
+ }  
+ 
+
   
 
 
   return (
-        <Box h='100%' bg='#fff'  justifyContent= 'center'>
-      <ScrollView h='10%'  justifyContent= 'center'  >
-     
-        
-        <Box  h='100%'  bg='#fff' >
+       
+      <ScrollView  bg='#fff'  pt={5} >
+     <VStack  alignItems='center' alignContent='center'pb={40}  >
+     <Box   width='100%' bg='#fff' pb={40}>
           
-            <Center>
-  <VStack  width="90%" mx="3" maxW="300px"  >
-  <HStack alignItems='center' justifyContent='center'>
-      <Box h="150"
-      w="150" 
-      borderColor= "#158e73"
-      borderBottomWidth='8px'
-      borderTopWidth='1px'
-      bg="#F8FAFB"
-      borderRadius={100} > 
-      
-      {/* Dynamically render image if user has uploaded a pic or display default avatar  */}
-    {image ? (<Image  alignItems="center" justifyContent="center" alignSelf='center' alignContent='center'
-      source= {{uri: image}}
-      alt="User Aatar"
-      flex={1}
-      w='140'
-      h='120'
-      resizeMode="cover"
-      borderRadius={100}
-      borderColor= "#e5e6ea"
-      borderWidth= "3px"
-    />):
-    // Avatar when there is no profile pic
-       (
-        <View w='80%'
-    h='80%' alignItems="center" justifyContent="center" alignSelf='center' alignContent='center' m='auto'>
-      <Image  alignItems="center" justifyContent="center" alignSelf='center' alignContent='center'
-    source= {require('../assets/avatar.png')}
-    alt="User Aatar"
-    flex={1}
-    w='140'
-    h='120'
-    resizeMode="contain"
-    borderRadius={100}
-    borderColor= "#e5e6ea"
-    borderWidth= "3px"
-  />
-  </View>
-  )
-  }  
+          <Center>
+<VStack  width="90%" mx="3" maxW="300px"  >
+<HStack alignItems="center" justifyContent='center'>
+     <Box h="150"
+    w="150" 
+    borderColor= "#158e73"
+    borderBottomWidth='8px'
+    borderTopWidth='1px'
+    bg="#eff3f6"
     
-    </Box>
+    borderRadius={100} >  
+    
+    
+    {/* Dynamically render image if user has uploaded a pic or display default avatar  */}
+    <Avatar bg="gray.100" alignSelf="center" justifyContent='center'mt={2} size="2xl"
+    source= {userImgSrc !=null ?{uri:userImgSrc} :imgSrc}
+    >
+        avatar
+      </Avatar>
+   </Box> 
 
-    {/* Camera Icon by the Picture border to handle onPress to select picture */}
-    <TouchableOpacity  onPress={pickImage} >
-    <Image 
-     source={require("../assets/camera.png")}
-     alt="Upload Image"
-     h="50"
-     w="50"
-     ml={-7}
-     mt={22}
-     position='absolute'
-    opacity={0.6}
-     resizeMode="contain"/>
-    </TouchableOpacity>
-    </HStack>
-  {/* <Box alignSelf='center' pb={5}>
-  <Image  source={require('../assets/icon.png')}
-             alt='headerImage' h='130' w='130' resizeMode="contain"/>
-             </Box> */}
-             
-        <FormControl isRequired >
+  {/* Camera Icon by the Picture border to handle onPress to select picture */}
+  <TouchableOpacity  onPress={pickImage} >
+    <Box position='absolute' mt={3} ml={-6} >
+  <Ionicons 
+        name='camera' type= "Octicons"size={35} color='#158e73' opacity={0.9} />
+  </Box>
+  </TouchableOpacity>
+  </HStack>
+{/* <Box alignSelf='center' pb={5}>
+<Image  source={require('../assets/icon.png')}
+           alt='headerImage' h='130' w='130' resizeMode="contain"/>
+           </Box> */}
+           
+      <FormControl mt='4' isRequired >
+      <FormControl.Label ><Text color='text.600' fontSize='11'>Phone Number</Text></FormControl.Label>
+        <Input mt='-2' mb='3' zIndex='-999' rounded="md"  type ='text' borderColor='#e5e6ea' keyboardType='number-pad' _input={{bg:'#F8FAFB'}} _focus={{
       
-          <Input mt='4' rounded="md" type ='text' borderColor='#e5e6ea' keyboardType='number-pad' _input={{bg:'#F8FAFB'}} _focus={{
-        
-        borderColor: "#158e73",
-        borderWidth: "1px"
-      }}  
+      borderColor: "#158e73",
+      borderWidth: "1px"
+    }}  
+    
+    value={number}
+    placeholder={data ? data.number|| 'add a phone number':'add a phone number'}
+    onChangeText={(number) => {
+    
+        setNumber(number);
       
-      placeholder="Number" 
-      onChangeText={(number) => setNumber(number)}
+    }}
+    />
+      </FormControl>
+
+      <FormControl isRequired >
+      <FormControl.Label><Text color='text.600' fontSize='11'>Handwork</Text></FormControl.Label>
+    <Input  mt='-2' mb='3' rounded="md" type ='text' borderColor='#e5e6ea' _input={{bg:'#F8FAFB'}} _focus={{
+      borderColor: "#158e73",
+      borderWidth: "1px"
+       }}  
+      value={skill}
+      placeholder={data ? data.skill|| 'What do you do':'you get handwork?'}
+      onChangeText={(skill) => setSkill(skill)}
       />
-        </FormControl>
+  </FormControl>
 
-        <FormControl isRequired >
-      <Input mt='4' rounded="md" type ='text' borderColor='#e5e6ea' _input={{bg:'#F8FAFB'}} _focus={{
-        borderColor: "#158e73",
-        borderWidth: "1px"
-         }}  
+  <FormControl mt='1' rounded="md">
+  <FormControl.Label  mb='-0.5'><Text color='text.600' fontSize='11'>Location</Text></FormControl.Label>
+  <DropDownMenu onSelectState={handleStateSelection} location={location} onSelectCapital={handleCapitalSelection} />
+   
+  </FormControl>
   
-        placeholder="Skill" 
-        onChangeText={(skill) => setSkill(skill)}
-        />
-    </FormControl>
+  <FormControl mt='4' importantForAccessibility='About'>
+    <FormControl.Label > <Text color='text.600' fontSize='11'>About Me</Text></FormControl.Label>
+  <TextArea mt='-2'  rounded="md" type ='text' InputRightElement={<Text>  {maxLength - about.length}</Text>}  borderColor='#e5e6ea'_input={{bg:'#F8FAFB'}} _focus={{
+      borderColor: "#158e73",
+      borderWidth: "1px"
+       }}  
+      value={about}
+      maxLength={maxLength}
+      placeholder={data ? data.about|| 'who you be':'tell us small thing about u'}
+      onChangeText={(about) => {if (about.length <= maxLength) {
+        setAbout(about);
+      }
+      }} />
+    {/* <Input mt='4' rounded="md" type ='text'   borderColor='#e5e6ea'_input={{bg:'#F8FAFB'}} _focus={{
+      borderColor: "#158e73",
+      borderWidth: "1px"
+       }}  
+      value={about}
+      placeholder={data ? data.about|| 'who you be':'tell us small thing about u'}
+      onChangeText={(about) => setAbout(about)}
+      /> */}
+  </FormControl>
+  
+      <Button rounded="md"  mt="5" bg='#158e73' colorScheme='emerald' onPress={imageUpload}>
+        Save
+      </Button>
+     
+        </VStack>
+        </Center>
+             </Box>
+   
 
-    <FormControl isRequired >
-      <Input mt='4' rounded="md" type ='text'  borderColor='#e5e6ea' _input={{bg:'#F8FAFB'}} _focus={{
-        borderColor: "#158e73",
-        borderWidth: "1px"
-         }}  
-  
-        placeholder="Location" 
-        onChangeText={(location) => setLocation(location)}
-        />
-    </FormControl>
+                 </VStack>
 
-    <FormControl isRequired >
-      <Input mt='4' rounded="md" type ='text'   borderColor='#e5e6ea'_input={{bg:'#F8FAFB'}} _focus={{
-        borderColor: "#158e73",
-        borderWidth: "1px"
-         }}  
-  
-        placeholder="About Me" 
-        onChangeText={(about) => setAbout(about)}
-        />
-    </FormControl>
-        <Button rounded="md"  mt="5" bg='#158e73' colorScheme='emerald'>
-          Save
-        </Button>
-      
-          </VStack>
-          </Center>
-               </Box>
+        
+
+
+
+
+
+
+
+
+
+
+       
        
       </ScrollView>
-  </Box>
+  
    
   
     
