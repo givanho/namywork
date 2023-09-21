a4b86ad457695c4c7d399d36963a501988073a3f
Givanho <fynefaceg@gmail.com>
2023-09-21 12:15:42 +0100

    direct message screen

--- Screens/MessageMenu.js
+++ Screens/MessageMenu.js
@@ -1,7 +1,7 @@
 
-import { View,  } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 import React, {useEffect, useCallback, useState, useLayoutEffect} from 'react'
-import { GiftedChat } from 'react-native-gifted-chat'
+import { GiftedChat, Send, Bubble, InputToolbar } from 'react-native-gifted-chat'
 import {ScrollView, Box, Image, Center ,Text, Input,Button,
   AspectRatio, Stack,HStack,Flex,VStack, Spacer, Link,KeyboardAvoidingView,
   FormControl,Heading} from 'native-base'
@@ -9,6 +9,7 @@ import Messages from '../bottom tabs/Messages'
   import {
     collection,
     setDoc,
+    where,
     addDoc,
     orderBy,
     doc,
@@ -20,19 +21,51 @@ import { UserAuth } from "../context/context";
 
 import LoadState from '../Components/LoadState';
 import LottieView from 'lottie-react-native';
-
+import { Ionicons } from '@expo/vector-icons'
+import { Feather } from '@expo/vector-icons'
 const MessageMenu = ({navigation, route}) => {
-  const {  fname, fprofilePic, fpostAuthorID} = route.params
+  const {chatRoomKeys , fpostAuthorID, receiverIDs,
+    receiverNames, receiverImgs, fname, fprofilePic} = route.params
   const {user} = UserAuth();
   const userid = user.uid
-const [isLoading, setIsLoading] = useState(false)
+const [imgSrc, setImgSrc] = useState('')
+const [userName, setUserName] = useState('')
 
   const [messages, setMessages] = useState([])
-const chatRoomKey = [userid, fpostAuthorID].sort().join('_');
-  useLayoutEffect(() => {
-setIsLoading(true)
-    const chatRoomRef = collection(db, 'chats','messages',  chatRoomKey);
+const chatRoomKey = [ userid, fpostAuthorID].sort().join('_');
 
+useEffect(() => {
+  if (user) {
+    //When the query snapshot changes (new data is added), 
+   // the onSnapshot callback function is called. If the query snapshot is not empty, 
+   // we update the state with the data from the first document in the snapshot.
+
+    const q = query(collection(db, 'users'), where('userID', '==', user.uid));
+
+    //The unsubscribe function returned by onSnapshot is used to 
+    //remove the listener when the component unmounts, preventing memory leaks.
+
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      if (!querySnapshot.empty) {
+        const doc = querySnapshot.docs[0];
+        setImgSrc(doc.data().userImg);
+        setUserName(doc.data().firstname + ' '+ doc.data().surname)
+        
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }
+}, [user])
+
+
+
+
+  useLayoutEffect(() => {
+    const chatRoomRef = collection(db, 'chats','messages',  chatRoomKeys||chatRoomKey);
+console.log('chatRoomKey', chatRoomKeys)
   const q = query(chatRoomRef, orderBy('createdAt', 'desc'));
 
   const unsubscribe = onSnapshot(q, (querySnapshot) => {
@@ -45,12 +78,11 @@ setIsLoading(true)
       }))
     );
   });
-  setIsLoading(false)
   return unsubscribe;
-}, [ route]);
+}, [chatRoomKey, route]);
 console.log('received messages', messages)
 
-const chatRoomRef = collection(db, 'chats','messages', chatRoomKey);
+const chatRoomRef = collection(db, 'chats','messages', chatRoomKeys||chatRoomKey);
 
 const onSend = useCallback((messages = []) => {
   setMessages((previousMessages) =>
@@ -64,6 +96,7 @@ const onSend = useCallback((messages = []) => {
     _id,
     createdAt,
     text,
+   
     user,
   });
 
@@ -75,46 +108,120 @@ const onSend = useCallback((messages = []) => {
       console.log("Document written with ID: ", docRef.id);
     })
     .catch((error) => {
-      console.error("Error adding document: ", error);
+      console.error("Error adding document:", error);
     });
 
 }, [ route]);
 
+const renderSend = (props) =>{
+  return (
+    <Send {...props}>
+<View >
+  <Feather name='send' size={26} color='#36454F'style={{
+    marginRight:7,
+    marginBottom:9,
+    justifyContent:'center',
+   
+  }} />
+</View>
+    </Send>
+  )
+}
+
+const renderBubble = (props) =>{
+  return (
+    <Bubble 
+    {...props}
+    wrapperStyle={{
+      right:{
+        backgroundColor:'#D3F5E4',
+        maxWidth: "80%",
+    
+    
+      },
+    
+      left:{
+        backgroundColor:'#fff',
+        borderWidth:StyleSheet.hairlineWidth,
+        borderColor:'#71797E',
+        maxWidth: "80%",
+      }
+    }}
+    textStyle={{
+      right:{
+        fontFamily:'Poppins-Regular',
+        color:'#000',
+        fontSize: 14,
+        lineHeight: 20,
+      },
+      left:{
+        fontFamily:'Poppins-Regular',
+        color:'#000',
+        fontSize: 14,
+        lineHeight: 20,
+      }
+    }}  
+
+    />
+      
+ 
+  )
+
+}
+const inputProps = (props) =>{
+  return (
+    <InputToolbar {...props}
+   containerStyle={{
+    backgroundColor:'#fff',
+    
+    borderTopWidth:StyleSheet.hairlineWidth,
+    borderBottomWidth:StyleSheet.hairlineWidth,
+    borderColor:'#71797E'
+   }}
+    />
+  )
+}
 
   return (
-    <View style={{ height:'100%', width:'100%'}}>
-      {route.params?  
-        ( <LoadState
-         showAnimation={isLoading == true}
-         title={'Loading...'}
-         source={require('../assets/animation/works.json')}
-       >
+    <View style={{ height:'100%', width:'100%', }}>
          <GiftedChat
         messages={messages}
         showAvatarForEveryMessage={false}
         showUserAvatar={false}
+
         onSend={messages => onSend(messages)}
         messagesContainerStyle={{
-          backgroundColor: '#fff'
+          backgroundColor: '#f8f8f8',
+          paddingBottom:30
         }}
         textInputStyle={{
-          backgroundColor: '#fff',
-          borderRadius: 20,
+          fontFamily: 'Poppins-Regular',
+          
         }}
+         renderSend={renderSend}
+        renderBubble={renderBubble}
+        scrollToBottom={true}
+        timeTextStyle={{right:{
+           color:'#000'
+                      },
+          }}
+          
+        alwaysShowSend={true}
+        renderInputToolbar={inputProps}
+
         user={{
           _id: user?.uid,
-          avatar: fprofilePic
+          name:userName,
+          receiverID:receiverIDs||fpostAuthorID,
+        receiverName: receiverNames||fname,
+        receiverImg:receiverImgs||fprofilePic,
+        chatRoom: chatRoomKeys||chatRoomKey,
+          avatar: imgSrc
         }}
       /> 
 
-      <Text> hi there is a param</Text>
-      </LoadState>
-      )
-      : 
-        (
-         <Messages chatRoomKey={chatRoomKeys}/> // Render ChatMenu when accessed through the bottom tab press
-       )
-       }
+    
+       
     
     </View>
 
