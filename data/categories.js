fc85dcb35e7975b276561e35d5b7d09338194c52
Givanho <fynefaceg@gmail.com>
2023-09-24 02:09:33 +0100

    category list

---
+++ data/categories.js
@@ -0,0 +1,90 @@
+const categories = [
+    {
+        id:1,
+        title:"Carpenter"
+    },
+    {
+        id:2,
+        title:"Hair Stylist"
+    },
+    {
+        id:3,
+        title:"Plumber"
+    },
+    {
+        id:4,
+        title:"Floor Tiling & P.O.P Installation"
+    },
+    {
+        id:5,
+        title:"Tailoring & Fashion"
+    },
+    {
+        id:6,
+        title:"Event Planner"
+    },
+    {
+        id:7,
+        title:"Photographer"
+    },
+    {
+        id:8,
+        title:"Food & Restaurant"
+    },
+    {
+        id:9,
+        title:"Auto-Mechanic"
+    },
+    {
+        id:10,
+        title:"Wristwatch Repair"
+    },
+    {
+        id:11,
+        title:"Phone and Laptop Repair"
+    },
+    {
+        id:12,
+        title:"HandBag, Beads, Crotchet making etc"
+    },
+    {
+        id:13,
+        title:"CCTV Installation"
+    },
+    {
+        id:14,
+        title:"Instrumentalist"
+    },
+    {
+        id:15,
+        title:"Make-Up, Nails and Eyelashes"
+    },
+    {
+        id:16,
+        title:"Solar Power Installation"
+    },
+    {
+        id:17,
+        title:"Painter"
+    },
+    {
+        id:18,
+        title:"Wallpaper Installation"
+    },
+    {
+        id:19,
+        title:"Alluminum Doors & Windows Installation"
+    },
+    {
+        id:20,
+        title:"Welder"
+    },
+    {
+        id:21,
+        title:"Gardener"
+    }
+
+
+]
+
+export default categories
