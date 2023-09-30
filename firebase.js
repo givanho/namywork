8a4a5ff851eac6a9534a120b94bea5539eefde56
Givanho <fynefaceg@gmail.com>
2023-09-30 04:12:23 +0100

    code clean up

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-
+import { getStorage } from "firebase/storage";
 
 import {FIRE_API,FIRE_AUTH,FIRE_PROJECT,
     FIRE_STORAGE,FIRE_MESSAGING,
@@ -25,3 +25,4 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app)
+export const storage = getStorage(app);
